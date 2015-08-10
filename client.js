c4e91407cb447a50aac3cf7bce6dc2de00fc1228
Marcel Klehr <mklehr@gmx.net>
2015-08-10 19:07:54 +0200

    let there be :bulb:

---
+++ client.js
@@ -0,0 +1,86 @@
+/**
+ * hive.js
+ * Copyright (C) 2013-2015 Marcel Klehr <mklehr@gmx.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var jsonParse = require('json-stream')
+  , through = require('through2')
+  , co = require('co')
+  , vdom = require('virtual-dom')
+  , h = vdom.h
+
+module.exports = setup
+module.exports.consumes = ['ui', 'editor']
+function setup(plugin, imports, register) {
+  var ui = imports.ui
+
+  var link = document.createElement('link')
+  link.setAttribute('rel', 'stylesheet')
+  link.setAttribute('href', 'static/hive-plugin-presence/css/index.css')
+  document.head.appendChild(link)
+
+  ui.page('/:id',
+  function loadClient(ctx, next) {
+    var container = document.createElement('div')
+    container.setAttribute('class', 'Presence')
+    document.body.insertBefore(container, document.body.firstChild)
+
+    var broadcast = ctx.broadcast.createDuplexStream(new Buffer('presence'))
+      , users = {} // model
+
+    broadcast.pipe(jsonParse()).pipe(through.obj(function(list, enc, cb) {
+      // Update model
+      users = list
+
+      // Update display
+      var newTree = render()
+        , patches = vdom.diff(tree, newTree)
+      vdom.patch(root, patches)
+      tree = newTree
+      cb()
+    }))
+
+    var root
+    var tree = render()
+    container.appendChild(root = vdom.create(tree))
+
+    function render() {
+      return h('div', [
+        h('div.Presence__Title', 'Users'),
+        h('ul.Presence__Users', Object.keys(users).map(function(userId) {
+          return renderUser(ctx, users[userId])
+        }))
+      ])
+    }
+
+    next()
+  })
+
+  register()
+}
+
+function renderUser(ctx, user) {
+  return h('li.Presence__User', [
+    h('span.Presence__User__name', user.name)
+  ])
+}
+
+
+function jsonStringify() {
+  return through.obj(function(buf, enc, cb) {
+    this.push(JSON.stringify(buf)+'\n')
+    cb()
+  })
+}
---
+++ index.js
@@ -0,0 +1,85 @@
+/**
+ * hive.js
+ * Copyright (C) 2013-2015 Marcel Klehr <mklehr@gmx.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var path = require('path')
+  , co = require('co')
+  , through = require('through2')
+  , JSONParse = require('json-stream')
+
+module.exports = setup
+module.exports.consumes = ['assets', 'broadcast', 'auth']
+
+function setup(plugin, imports, register) {
+  var assets = imports.assets
+    , broadcast = imports.broadcast
+    , auth = imports.auth
+
+  assets.registerModule(path.join(__dirname, 'client.js'))
+  assets.registerStaticDir(path.join(__dirname, 'css'))
+
+  var users = {}
+
+  broadcast.registerChannel(new Buffer('presence'), function(user, docId, clientStream, broadcastStream) {
+    // update local list
+    users[user.id] = user
+
+    var broadcasting = JSONStringify()
+    broadcasting.pipe(broadcastStream)
+
+    // Send the new list to everyone
+    broadcasting.write(users)
+
+    var thisClient = JSONStringify()
+    thisClient.pipe(clientStream)
+
+    // Send the new list to this client
+    setTimeout(function() {
+      thisClient.write(users)
+    }, 3000)
+
+    // Notify the others if this client disconnects
+    clientStream.on('close', function() {
+      delete users[user.id]
+      broadcasting.write(users)
+    })
+
+    // Throw away incoming messages
+    clientStream.pipe(through.obj(function(buf, enc, cb){
+      cb()
+    }))
+
+    // Athorize presence:read
+    broadcastStream.pipe(through(function(buf, enc, cb) {
+      var that = this
+      co(function*() {
+        var authorized = yield auth.authorize(user, 'document/presence:read', {document: docId})
+        if(!authorized) return
+        that.push(buf)
+      }).then(cb).catch(cb)
+    })).pipe(clientStream)
+  })
+
+  register()
+}
+
+
+function JSONStringify() {
+  return through.obj(function(buf, enc, cb) {
+    this.push(JSON.stringify(buf)+'\n')
+    cb()
+  })
+}
